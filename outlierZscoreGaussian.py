280fb1ecaffc855d3dd8eca424a379c2ecb9a046
vasu018 <vasu.018@gmail.com>
2018-09-18 16:38:09 -0400

    Code documentation and code debug printing messages.

--- outlierZscoreGaussian.py
+++ outlierZscoreGaussian.py
@@ -1,17 +1,44 @@
-# Import required packages
-# from sklearn import preprocessing
+'''
+Module to calculate the outliers for network configurations that is supplied
+as features. Outliers are calculated on the multi-class features using the
+zScore and modified zScore Outlier analyzer technique.
+
+<Input>: Type of question and parameters to the question.
+High level design:
+
+Step 1: From the data frames generated as output of the question is converted to
+multi-class feature.
+
+Step 2: The multi-class is feature is encoded using the oneHot Encoding or
+MultiLabel Binarizer.
+
+Step 3: The encoded data is converted to some distribution using techniques such as:
+    (a) rough clustering and
+    (b) ...
+
+Step 4: The distribution is supplied to different outlier Analyzer techniques for
+Outlier detection:
+    (a) zScore
+    (b) modified zScore
+    (c) ...
+<Output>: Outliers
+'''
+
 import pandas as pd
 import numpy as np
-from sklearn import preprocessing
 from sklearn.preprocessing import LabelBinarizer, MultiLabelBinarizer
-
-# importing Statistics module
 import statistics
 import numpy
 
-#
-# Create multiclass feature
-#
+DEBUG_PRINT_FLAG = True
+
+'''
+Example list of multiclass features.
+
+[TODO]: These static features will be extracted from the pybatfish questions 
+data frames.
+'''
+
 multiclass_feature = [("1.1.1.1", "2.2.2.2", "1500"),
                       ("1.1.1.1", "3.3.3.3", "1500"),
                       ("3.3.3.3", "", "9100"),
@@ -45,41 +72,45 @@ multiclass_feature2 = [("1.1.1.1", "2.2.2.2"),
                       ("", ""),
                       ("1.1.1.1", ""),
                       ("2.2.2.2","")]
+if DEBUG_PRINT_FLAG:
+    # Input Multi-class feature.
+    print("# Multi-class feature:\n", multiclass_feature)
+    print()
 
-# Input Multi-class feature.
-print("Multi-class feature:\n", multiclass_feature)
-
-# Create multiclass one-hot encoder
+# Create multiclass multilabelbinarizer
 one_hot_multiclass = MultiLabelBinarizer()
-
-# One-hot encode multiclass feature
 multiClassEncodedList = one_hot_multiclass.fit_transform(multiclass_feature)
-print("Multi-class encoding:\n", multiClassEncodedList)
 
-#
-# Debug print messages
-#
+if DEBUG_PRINT_FLAG:
+    print("# Multi-class encoded features:\n", multiClassEncodedList)
+    print()
+
 uniqueClasses = one_hot_multiclass.classes_
-print("Unique Classes:", uniqueClasses)
 
-# Calculate the count of each class
-# i.e., count of column elements in the encoded multi-class array.
+if DEBUG_PRINT_FLAG:
+    print("# Unique Classes:", uniqueClasses)
+    print()
+
+'''
+Count the frequency of specific attribute value in the complete data that is 
+constructed using the  MultiLabelBinarizer(). 
+'''
 mClassElementCountList = []
 uniqueClassesNonNull = []
 for counter, mClass in enumerate(one_hot_multiclass.classes_):
-    # if (uniqueClasses[counter] == ''):
-    #     continue
-
-    # print(mClass)
     uniqueClassesNonNull.append(mClass)
     mClassDensityValue = 0
     for multiClassElementCode in multiClassEncodedList:
         mClassDensityValue = mClassDensityValue + multiClassElementCode[counter]
     mClassElementCountList.append(mClassDensityValue)
 
-print("Each class count:", mClassElementCountList)
+if DEBUG_PRINT_FLAG:
+    print("# Each class count:", mClassElementCountList)
+    print()
 
-# Calculate the density of each element in the data list Xi
+''' 
+Calculate the density of each attribute value in the data entries Xi.
+'''
 entryDensityListNormalize = []
 entryDensityList = []
 for classCounter, entryVector in enumerate(multiClassEncodedList):
@@ -89,43 +120,55 @@ for classCounter, entryVector in enumerate(multiClassEncodedList):
         if (uniqueClasses[entryCounter] == ''):
             continue
         summationVectorVals = summationVectorVals + (entryVectorClassValue * mClassElementCountList[entryCounter])
-        # print(entryCounter)
     densityXi = overallProportion * summationVectorVals
     entryDensityListNormalize.append(densityXi)
     entryDensityList.append(summationVectorVals)
 
-print("Density Values list:\n", entryDensityListNormalize)
-print("Density Values list (Non-normalize):\n", entryDensityList)
+if DEBUG_PRINT_FLAG:
+    print("# Density Values list of data-set (Normalized):\n", entryDensityListNormalize)
+    print()
+    print("# Density Values list of data-set (Real):\n", entryDensityList)
+    print()
 
 
-# Calculate Mean and Standard deviation on the density values of data set.
+''' 
+Calculate Mean and Standard deviation on the density values of data set.
+'''
 meanDataSet =  numpy.mean(entryDensityList)
 medianDataSet = numpy.median(entryDensityList)
 standardDeviationDataSet = statistics.stdev(entryDensityList)
-print("# Mean:", meanDataSet)
-print("# Standard Deviation:", standardDeviationDataSet)
+if DEBUG_PRINT_FLAG:
+    print("# Mean:", meanDataSet)
+    print("# Standard Deviation:", standardDeviationDataSet)
 
-# Calculate ZScores and Outliers.
+''' 
+Calculate ZScores and Outliers.
+'''
+if DEBUG_PRINT_FLAG:
+    print()
+    print("## Actual Value \t | zScore (Mean) \t | zScore (Median)")
+
 outliersMean = []
 outliersMedian = []
-
 for entryDensityCounter, eachValue in enumerate(entryDensityList):
     zScoreMean = abs((eachValue - meanDataSet) / (standardDeviationDataSet))
     zScoreMedian = abs((eachValue - medianDataSet) / (standardDeviationDataSet))
 
-    # Debug message
-    # print("Actual Value:",eachValue, "zScore:", zScoreMean)
-    print("Actual Value:",eachValue, "zScore:", zScoreMedian)
+    if DEBUG_PRINT_FLAG:
+        print("\t", eachValue,"\t\t\t\t", zScoreMean,"\t\t", zScoreMedian)
 
-    if (zScoreMean >= 0.9):
+    # Currently one standard deviation is considered for calculating the outliers.
+    if (zScoreMean >= 1):
         outliersMean.append(multiclass_feature[entryDensityCounter])
 
-    if (zScoreMedian >= 0.9):
+    if (zScoreMedian >= 1):
         outliersMedian.append(multiclass_feature[entryDensityCounter])
 
 
-print("# Outliers with mean:")
-print(outliersMean)
-
-print("# Outliers with Median:")
-print(outliersMedian)
+if DEBUG_PRINT_FLAG:
+    print()
+    print("# Outliers with Mean on data-set:")
+    print(outliersMean)
+    print()
+    print("# Outliers with Median on data-set:")
+    print(outliersMedian)
