b78b2ff4e2b6b8b5286a43896bd37da6fb0a2e4b
Sankalp Suhas Taralekar <sankalpsuhastaralekar@Sankalps-MacBook-Pro.local>
2018-10-24 20:06:37 -0400

    Pushing the Gaussian mixture after resolving the comments

--- Gaussian_network.py
+++ Gaussian_network.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from sklearn import mixture
 from sklearn.preprocessing import LabelBinarizer, MultiLabelBinarizer
+import math
 
 multiclass_feature_Xi = [("1.1.1.1", "2.2.2.2"),
                       ("1.1.1.1", "3.3.3.3"),
@@ -24,19 +25,19 @@ multiclass_feature_Xi = [("1.1.1.1", "2.2.2.2"),
 
 one_hot_multiclass = MultiLabelBinarizer()
 multiClassEncodedList = one_hot_multiclass.fit_transform(multiclass_feature_Xi)
-print("first list is")
+print("first feature encoded is")
 print(multiClassEncodedList)
-l1 = list(multiClassEncodedList)
+feature_1 = list(multiClassEncodedList)
 
+#Gaussian Mixture is used for soft clustering. Insted of assigning points to specific classes it assigns probability.
+#The n_components parameter in the Gaussian is used to specify the number of Gaussians.
 clf = mixture.GaussianMixture(n_components=1, covariance_type='full')
-#clf.fit(X_train)
 clf.fit(multiClassEncodedList)
 Z = -clf.score_samples(np.array(multiClassEncodedList))
 print("the scores given by GMM when components is 1 are as follows:")
 print(Z)
 
 clf = mixture.GaussianMixture(n_components=2, covariance_type='full')
-#clf.fit(X_train)
 clf.fit(multiClassEncodedList)
 Z = -clf.score_samples(np.array(multiClassEncodedList))
 print("the scores given by GMM when components is 2 are as follows:")
@@ -44,7 +45,6 @@ print(Z)
 
 
 clf = mixture.GaussianMixture(n_components=3, covariance_type='full')
-#clf.fit(X_train)
 clf.fit(multiClassEncodedList)
 Z = -clf.score_samples(np.array(multiClassEncodedList))
 print("the scores given by GMM when components is 3 are as follows:")
@@ -70,33 +70,32 @@ NTP_Servers = [("1.1.1.1", "2.2.2.2"),
 
 one_hot_multiclass = MultiLabelBinarizer()
 multiClassEncodedList2 = one_hot_multiclass.fit_transform(NTP_Servers)
-print("second list is")
+print("second feature encoded is")
 print(multiClassEncodedList2)
-l2 =list(multiClassEncodedList2)
+feature_2 =list(multiClassEncodedList2)
 
 
-list3 = [list(a) for a in zip(l1, l2)]
-for i in range(len(list3)):
+concatenated_features= [list(a) for a in zip(feature_1, feature_2)]
+for i in range(len(concatenated_features)):
     print("values are")
-    print(list3[i][0])
-print("concatented list is")
-print(list3)
+    print(concatenated_features[i][0])
+print("concatented features are")
+print(concatenated_features)
 
-for i in range(len(list3)):
-    list3[i][0]=list3[i][0].tolist()
-    list3[i][1] = list3[i][1].tolist()
-
-print(list3)
-
-for i in range(len(list3)):
-    list3[i]=list3[i][0] + list3[i][1]
+for i in range(len(concatenated_features)):
+    concatenated_features[i][0]=concatenated_features[i][0].tolist()
+    concatenated_features[i][1] = concatenated_features[i][1].tolist()
 
 
-print(list3)
+
+for i in range(len(concatenated_features)):
+    concatenated_features[i]=concatenated_features[i][0] + concatenated_features[i][1]
+
+
+
 clf = mixture.GaussianMixture(n_components=2, covariance_type='full')
-clf.fit(list3)
-#clf.fit(list3)
-Z = -clf.score_samples(np.array(list3))
+clf.fit(concatenated_features)
+Z = -clf.score_samples(np.array(concatenated_features))
 print("log likeloihood scores are")
 print(Z)
 
