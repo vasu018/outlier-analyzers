0d22e649587d1e409e5ad4b55e3149d1099221c9
Alfred Wu <alfredwu@Alfreds-MacBook-Pro.local>
2018-12-15 00:12:24 -0500

    Add support for definitions.

--- outlierAnalyzer.py
+++ outlierAnalyzer.py
@@ -34,6 +34,7 @@ import outlierLibrary
 from collections import Counter
 import json
 from pprint import pprint
+import re
 
 # Importing pybatfish APIs. 
 from pybatfish.client.commands import *
@@ -51,6 +52,9 @@ bf_init_snapshot('datasets/networks/example')
 
 # Debug flags
 DEBUG_PRINT_FLAG = True
+
+# Option flag
+DEFINITION = True
 
 # Static Threshold for comparison with density calcuation
 OUTLIER_THRESHOLD = 1.0 / 3.0
@@ -122,19 +126,94 @@ if READ_FILE_FLAG:
 
 else:
 
-    # Or read the question and property from the command line and parse the returned data frame
-    command = "result = bfq." + sys.argv[2] + ".answer().frame()"
-    exec(command)
-    print(result)
+    if DEFINITION == False:
 
-    props = sys.argv[4].split('|')
-    for i in range(len(props)):
-        props[i] = props[i].strip()
+        # Or read the question and property from the command line and parse the returned data frame
+        command = "result = bfq." + sys.argv[2] + ".answer().frame()"
+        exec(command)
+        print(result)
 
-    datas = []
-    for prop in props:
-        data = listify(result[prop])
+        props = sys.argv[4].split('|')
+        for i in range(len(props)):
+            props[i] = props[i].strip()
+
+        datas = []
+        for prop in props:
+            data = listify(result[prop])
+            datas.append(data)
+
+
+    else:
+
+        props = []
+        pre_datas = []
+
+        f = open('datasets/flat-sample/namedStructureProperties_ip-accesslist.json')
+
+        count = 0
+        for line in f:
+
+            match = re.match('.*?:(.*)=>(.*);', line)
+
+            # props.append(match.group(1))
+    
+            extracted = match.group(2)
+            extracted = '[' + extracted + ']'
+
+            pre_data = json.loads(extracted)
+
+            for i in range(len(pre_data)):
+                # data[i] = str(data[i])
+                pre_data[i] = [pre_data[i]]
+
+            if count == 1:
+                pre_datas.append(pre_data)
+                props.append(match.group(1))
+
+            count += 1
+            if count == 2:
+                break
+
+
+        def extract_keys(the_dict, prefix=''):
+            # TODO
+            # fix bug with list of dicts not being extracted
+            # but only first element
+
+            key_list = []
+
+            for key, value in the_dict.items():
+
+                # set the prefix
+                if len(prefix) == 0:
+                    new_prefix = key
+                else:
+                    new_prefix = prefix + '.' + key
+
+                # recursively call extract_keys for nested dicts
+                if type(value) == dict:
+                    key_list.extend(extract_keys(value, new_prefix))
+                elif type(value) == list and type(value[0]) == dict:
+                    key_list.extend(extract_keys(value[0], new_prefix))
+                else:
+                    new_item = new_prefix + '{' + str(the_dict[key]) + '}'
+                    key_list.append(new_item)
+
+            return key_list
+
+
+        datas = []
+
+        for pre_data in pre_datas:
+            data = []
+
+            for d in pre_data:
+                data.append(extract_keys(d[0]))
+
         datas.append(data)
+
+
+
 
 
 # Encode using multi label binarizer and calculate frequency
